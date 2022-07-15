d9d825495f3d30e23669a5b7121ab8f2b97c1530
jhoukem <jhoukem@gmail.com>
2022-07-15 20:05:37 +0200

    fix(Http): fix the http lib by using unirest instead

--- src/main/kotlin/com/malt/hiringexercise/geoip/IpStackService.kt
+++ src/main/kotlin/com/malt/hiringexercise/geoip/IpStackService.kt
@@ -3,31 +3,28 @@ package com.malt.hiringexercise.geoip
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import org.springframework.http.HttpStatus
+import kong.unirest.Unirest
 import org.springframework.stereotype.Service
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse.BodyHandlers
 
 @Service
 class IpStackService : GeoIpService {
 
-    private final val httpClient: HttpClient = HttpClient.newHttpClient()
     private final val mapper: ObjectMapper = jacksonObjectMapper()
 
     override fun getCountryCode(ip: String): String? {
-        val request = HttpRequest.newBuilder(URI.create("https://api.ipstack.com/$ip?access_key=$API_KEY"))
-            .GET()
-            .build()
-        val response = httpClient.send(request, BodyHandlers.ofString())
 
-        if (response.statusCode() != HttpStatus.OK.value()) {
+        val httpResponse = Unirest.get("https://api.ipstack.com/{ip}")
+            .header("accept", "application/json")
+            .routeParam("ip", ip)
+            .queryString("access_key", API_KEY)
+            .asString()
+
+        if (!httpResponse.isSuccess) {
             return null
         }
 
         return try {
-            val ipStackResponse = mapper.readValue<IpStackResponse>(response.body())
+            val ipStackResponse = mapper.readValue<IpStackResponse>(httpResponse.body)
             ipStackResponse.country_code
         } catch (e: Exception) {
             null
