50e71b8d0eff0e1cd1b9d3abe1c6f0f2d6c9d000
jhoukem <jhoukem@gmail.com>
2022-07-15 16:54:04 +0200

    imp(CommissionRateController): update controller base path + remove useless try catch

--- src/main/kotlin/com/malt/hiringexercise/commissionrate/CommissionRateController.kt
+++ src/main/kotlin/com/malt/hiringexercise/commissionrate/CommissionRateController.kt
@@ -3,19 +3,17 @@ package com.malt.hiringexercise.commissionrate
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("commissions")
+@RestController
+@RequestMapping("commissions")
 class CommissionRateController constructor(private val commissionRateService: CommissionRateService) {
 
-    @PostMapping("/rate/compute")
+    @PostMapping("rate/compute")
     fun getCommission(@RequestBody commissionRateRequestDTO: CommissionRateRequestDTO): ResponseEntity<CommissionRateResponseDTO> {
-        return try {
-            val commission = commissionRateService.computeCommission(commissionRateRequestDTO);
-            ResponseEntity.ok(commission);
-        } catch (e: Exception) {
-            ResponseEntity.badRequest().build();
-        }
+        val commission = commissionRateService.computeCommission(commissionRateRequestDTO);
+        return ResponseEntity.ok(commission);
     }
 
 }
