021ceefffe429d9456658ee22fb427d0c9d71121
jhoukem <jhoukem@gmail.com>
2022-07-15 20:06:25 +0200

    fix(CommissionRule): fix the commission rule creation using a polymorphic deserializer

--- src/main/kotlin/com/malt/hiringexercise/commission/CommissionRule.kt
+++ src/main/kotlin/com/malt/hiringexercise/commission/CommissionRule.kt
@@ -1,6 +1,7 @@
 package com.malt.hiringexercise.commission
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonTypeName
 import java.time.Period
 
 data class CommissionRule(
@@ -13,9 +14,21 @@ data class Rate(val percent: Int)
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
 sealed class Restriction
-class Or(val restrictions: List<Restriction>) : Restriction()
-class And(val restrictions: List<Restriction>) : Restriction()
-class MissionDuration(val greaterThan: Period) : Restriction()
-class CommercialRelationDuration(val greaterThan: Period) : Restriction()
-class CustomerLocation(val countryCode: String) : Restriction()
-class FreelanceLocation(val countryCode: String) : Restriction()
+
+@JsonTypeName("or")
+data class Or(val restrictions: List<Restriction>) : Restriction()
+
+@JsonTypeName("and")
+data class And(val restrictions: List<Restriction>) : Restriction()
+
+@JsonTypeName("mission_duration")
+data class MissionDuration(val greaterThan: Period) : Restriction()
+
+@JsonTypeName("commercial_duration")
+data class CommercialRelationDuration(val greaterThan: Period) : Restriction()
+
+@JsonTypeName("customer_location")
+data class CustomerLocation(val countryCode: String) : Restriction()
+
+@JsonTypeName("freelance_location")
+data class FreelanceLocation(val countryCode: String) : Restriction()
