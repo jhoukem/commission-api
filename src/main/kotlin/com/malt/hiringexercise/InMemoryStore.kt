2999a9796bef2129306eb53492b653d3d20f48cd
jhoukem <jhoukem@gmail.com>
2022-07-15 12:19:33 +0200

    fix(InMemoryStore): add a missing import

--- src/main/kotlin/com/malt/hiringexercise/InMemoryStore.kt
+++ src/main/kotlin/com/malt/hiringexercise/InMemoryStore.kt
@@ -1,34 +1,31 @@
-package com.malt.hiringexercise;
+package com.malt.hiringexercise
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import kotlin.streams.toList
 
 /**
  * An example of how you could handle the "data part" of the exercise.
  */
 @Component
-public class InMemoryStore {
+class InMemoryStore {
+    private val things: MutableList<String> = ArrayList()
 
-    private final List<String> things = new ArrayList<>();
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void storeSomeThings(ApplicationReadyEvent e) {
-        addThing("foo");
-        addThing("bar");
-        addThing("foobar");
+    @EventListener(ApplicationReadyEvent::class)
+    fun storeSomeThings(e: ApplicationReadyEvent?) {
+        addThing("foo")
+        addThing("bar")
+        addThing("foobar")
     }
 
-    public void addThing(String newThing) {
-        things.add(newThing);
+    fun addThing(newThing: String) {
+        things.add(newThing)
     }
 
-    public List<String> findThingsHavingNameStartingWith(String namePrefix) {
+    fun findThingsHavingNameStartingWith(namePrefix: String?): List<String> {
         return things.stream()
-                .filter(n -> n.startsWith(namePrefix))
-                .toList();
+            .filter { n: String -> n.startsWith(namePrefix!!) }
+            .toList()
     }
 }
