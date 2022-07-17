17d25a09854b40b0c63c1ecdf8f8dce57415fe3e
jhoukem <jhoukem@gmail.com>
2022-07-17 19:34:23 +0200

    chore(test): use springboot HttpStatus instead of Unirest one in unit tests

--- src/test/kotlin/com/malt/hiringexercise/commission/CommissionControllerTest.kt
+++ src/test/kotlin/com/malt/hiringexercise/commission/CommissionControllerTest.kt
@@ -2,7 +2,6 @@ package com.malt.hiringexercise.commission
 
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import kong.unirest.HttpStatus
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.verify
@@ -10,6 +9,7 @@ import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
